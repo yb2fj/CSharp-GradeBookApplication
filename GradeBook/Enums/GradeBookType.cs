05e1eb6907be55ed74094cf3e1c3ecb696867690
Yuri Bang <ybang@silverchair.com>
2019-04-14 12:36:56 -0400

    changes

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace GradeBook.Enums
+{
+    public enum GradeBookType
+    {
+        Standard,
+        Ranked,
+        ESNU,
+        OnetoFour,
+        SixPoint
+    }
+}
---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,14 @@
+﻿
+
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook (string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
