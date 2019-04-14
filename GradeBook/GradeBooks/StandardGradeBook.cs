2bd70c3a0e5c8a9ab0e0011d15d6c3fabdbab2cb
Yuri Bang <ybang@silverchair.com>
2019-04-14 13:47:41 -0400

    module #7

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -5,7 +5,9 @@ namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
     {
-        public StandardGradeBook(string name) : base(name)
+
+      
+        public StandardGradeBook(string name, bool isWeighted) : base(name, isWeighted)
         {
             Type = GradeBookType.Standard;
         }
