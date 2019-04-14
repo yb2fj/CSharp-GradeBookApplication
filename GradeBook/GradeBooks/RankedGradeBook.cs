1ed8bdea491c76238c24690af4e6c5c89e4d5ca1
Yuri Bang <ybang@silverchair.com>
2019-04-14 13:12:25 -0400

    module #4

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,6 +13,34 @@ namespace GradeBook.GradeBooks
             Type = GradeBookType.Ranked;
         }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            
+                base.CalculateStatistics();
+            
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            
+            
+                base.CalculateStudentStatistics(name);
+            
+        }
+
+
+
         public override char GetLetterGrade (double averageGrade)
         {
             if (Students.Count < 5)
